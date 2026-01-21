9b298f1fc6c8cca183f752c6dc8c86a5893cb1fc
kijiwon <kiji1313@gmail.com>
2026-01-21 23:25:55 +0900

    zustand middleware-subscribeWithSelector

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,28 +1,44 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import { combine, subscribeWithSelector } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
-// immer
-// 속성에 직접 접근(불변성 관리) -> 업데이트 코드 간결
 export const UseCountStore = create(
-  immer(
-    // combine 사용하는 이유?
-    // state 타입 자동 추론(첫 번째 인수를 기준으로)
-    combine({ count: 0 }, (set) => ({
-      actions: {
-        increase: () => {
-          set((state) => {
-            state.count += 1;
-          });
+  subscribeWithSelector(
+    // immer
+    // 속성에 직접 접근(불변성 관리) -> 업데이트 코드 간결
+    immer(
+      // combine 사용하는 이유?
+      // state 타입 자동 추론(첫 번째 인수를 기준으로)
+      combine({ count: 0 }, (set) => ({
+        actions: {
+          increase: () => {
+            set((state) => {
+              state.count += 1;
+            });
+          },
+          decrease: () => {
+            set((state) => {
+              state.count -= 1;
+            });
+          },
         },
-        decrease: () => {
-          set((state) => {
-            state.count -= 1;
-          });
-        },
-      },
-    })),
+      })),
+    ),
   ),
+);
+
+// count 값 구독 -> 변경을 감지하면 콜백함수 실행
+UseCountStore.subscribe(
+  (store) => store.count,
+  (count, prevCount) => {
+    // Listener
+    console.log(count, prevCount);
+
+    // store 불러오기
+    // const store = UseCountStore.getState();
+    // store 업데이트
+    // UseCountStore.setState((store) => ({ count: 10 }));
+  },
 );
 
 export const useCount = () => {
