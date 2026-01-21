1678a4d12e1a8ca99ffd36a601dea34857a2e7cc
kijiwon <kiji1313@gmail.com>
2026-01-21 22:59:04 +0900

    zustand middleware-immer

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,24 +1,28 @@
 import { create } from "zustand";
 import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-// combine 사용하는 이유?
-// state 타입 자동 추론(첫 번째 인수를 기준으로)
+// immer
+// 속성에 직접 접근(불변성 관리) -> 업데이트 코드 간결
 export const UseCountStore = create(
-  combine({ count: 0 }, (set, get) => ({
-    actions: {
-      increase: () => {
-        get();
-        set((store) => ({
-          count: store.count + 1,
-        }));
+  immer(
+    // combine 사용하는 이유?
+    // state 타입 자동 추론(첫 번째 인수를 기준으로)
+    combine({ count: 0 }, (set) => ({
+      actions: {
+        increase: () => {
+          set((state) => {
+            state.count += 1;
+          });
+        },
+        decrease: () => {
+          set((state) => {
+            state.count -= 1;
+          });
+        },
       },
-      decrease: () => {
-        set((store) => ({
-          count: store.count - 1,
-        }));
-      },
-    },
-  })),
+    })),
+  ),
 );
 
 export const useCount = () => {
